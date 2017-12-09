c074fe2d7823c8bf0a75938d283e3d037715ff57
onedou <onedou@126.com>
2017-12-09 18:52:25 +0800

    update site select

--- src/pages/site/site.ts
+++ src/pages/site/site.ts
@@ -19,10 +19,12 @@ import {NotePage} from '../note/note';
 export class SitePage {
     userinfo: any;
     items: Array<any>;
-    selectIndex: number;
-    isLast: boolean;
-    siteListContent: Array<Element>;
-    siteItems: Array<Element>;
+
+    isLast: any        = {};
+    isSetListener: any = {};
+    selectIndex: any   = {};
+    list: any          = {};
+    canExecute:boolean = true;
 
     constructor(public navCtrl: NavController, public navParams: NavParams) {
         this.userinfo = this.navParams.get("userinfo");
@@ -36,50 +38,110 @@ export class SitePage {
         this.items.push("/testv2/tttt5");
         this.items.push("/testv2/tttt6");
         this.items.push("/testv2/tttt7");
+
+        this.selectIndex.mine   = null;
+        this.selectIndex.theirs = null;
     }
 
-    ionViewDidLoad() {
-        this.siteItems = Array.from(document.querySelectorAll(".site-item"));
-
-        this.siteItems.forEach((element: HTMLElement) => {
-            element.addEventListener("click", () => {
-                this.setAllItem();
-                element.style.marginTop = -element.parentElement.scrollTop + "px";
-            });
-        });
-
-        let siteListContent:HTMLElement = document.querySelector(".site-list-content");
-
-        siteListContent.onscroll = () => {
-            this.setAllItem();
-
-            if(this.selectIndex == this.siteItems.length - 1){
-                this.isLast = false;
-            }
-
-            this.selectIndex = null;
-        }
-    }
+    ionViewDidLoad() {}
 
     confirm(){
         this.navCtrl.push(NotePage,{});
     }
 
-    selectItem(index: number){
-        this.selectIndex = index;
+    selectItem(event: any, index: number, type: String): any{
+        this.setAllUnselected(true);
 
-        if(index == this.siteItems.length - 1){
-            this.isLast = true;
-        }else{
-            this.isLast = false;
+        if(type == "mine"){
+            this.selectIndex.mine = index;
+
+            if(index == this.items.length - 1){
+                this.isLast.mine = true;
+            }else{
+                this.isLast.mine = false;
+            }
+        }else if(type == "theirs"){
+            this.selectIndex.theirs = index;
+
+            if(index == this.items.length - 1){
+                this.isLast.theirs = true;
+            }else{
+                this.isLast.theirs = false;
+            }
+        }
+
+        event.path.forEach(element => {
+            if(element.tagName == "ION-ITEM"){
+                this.setListContentListener(element.parentElement, type);
+                this.setAllItemMarginTopNull(element.parentElement);
+                element.style.marginTop = -element.parentElement.scrollTop + "px";
+
+                if(type == "mine") {
+                    this.list.mine = element.parentElement;
+                }else if(type == "theirs"){
+                    this.list.theirs = element.parentElement;
+                }
+            }
+        });
+    }
+
+    setAllItemMarginTopNull(element: HTMLElement){
+        if(element){
+            let childNodes: Array<any> = Array.from(element.childNodes);
+            
+            childNodes.forEach(innerElement => {
+                if(innerElement.tagName == "ION-ITEM"){
+                    innerElement.style.marginTop = null;
+                }
+            });
         }
     }
 
-    setAllItem(){
-        let ionItem:Array<Element> = Array.from(document.querySelectorAll("ion-item"));
+    setListContentListener(element: HTMLElement, type: String){
+        let that = this;
+
+        function set(){
+            element.addEventListener("scroll", () => {
+                that.setAllItemMarginTopNull(element);
+    
+                if(that.selectIndex.mine == that.items.length - 1){
+                    that.isLast.mine = false;
+                }
+
+                if(that.selectIndex.theirs == that.items.length - 1){
+                    that.isLast.theirs = false;
+                }
+
+                that.setAllUnselected(false);
+            });
+        }
+
+        if(type == "mine" && !this.isSetListener.mine){
+            this.isSetListener.mine = true;
+            set();
+        }
         
-        ionItem.forEach((element: HTMLElement) => {
-            element.style.marginTop = null;
-        });
+        if(type == "theirs" && !this.isSetListener.theirs){
+            this.isSetListener.theirs = true;
+            set();
+        }
+    }
+
+    setAllUnselected(isFromItem: boolean){
+        if(isFromItem){
+            this.canExecute = false;
+
+            setTimeout(() => {
+                this.canExecute = true;
+            }, 10)
+        }
+
+        if(isFromItem || this.canExecute){
+            this.selectIndex.mine   = null;
+            this.selectIndex.theirs = null;
+
+            this.setAllItemMarginTopNull(this.list.mine);
+            this.setAllItemMarginTopNull(this.list.theirs);
+        }
     }
 }
